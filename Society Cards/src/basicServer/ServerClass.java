6b707be7b3910cd666de063ba77f2390c42089a9
O(n) Phelan <phelanowen@gmail.com>
2016-12-15 17:18:22 +0000

    Yeah, those server methods should do

--- Society Cards/src/basicServer/ServerClass.java
+++ Society Cards/src/basicServer/ServerClass.java
@@ -15,20 +15,38 @@ public class ServerClass {
 	
 	public void addStudentToSociety(int clientID, String societyName) {
 		/* TODO:
-		 * Check this is coming from an admin, then assign to clientInformation ArrayList of client 
+		 * checkIfAdmin(clientID)
+		 * true: then assign to clientInformation ArrayList of client
+		 * false: report hacker to the FBI (or do nothing) 
 		 */
 	}
 	
-	public ArrayList<String> checkAssignedSocieties(int clientID) {
+	public void removeStudentFromSociety(int clientID, String societyName) {
 		/* TODO:
-		 * clientInformation.getID or whatever it is
+		 * checkIfAdmin(clientID)
+		 * true: then remove from clientInformation ArrayList of client
+		 * false: eat doritos and cry about social anxiety (or nothing or whatever)
+		 */
+	}
+	
+	public ArrayList<String> getAssignedSocieties(int clientID) {
+		/* TODO:
+		 * clientInformation.getID or whatever it is, return the list of societies
 		 */
 		return null;
 	}
 	
-	public boolean checkIfAdmin(int clientID) {
+	public boolean checkIfAdmin(int clientID, String society) {
 		/* TODO:
-		 * adminTable.getID or whatever, check if it's in, return true/false
+		 * adminTable.getID or whatever, check if it's there for that society, return true/false
+		 */
+		return false;
+	}
+	
+	public boolean checkIfMember(int clientID, String societyName) {
+		/* TODO:
+		 * listOfSocieties = getAssignedSocieties(clientID);
+		 * if listOfSocieties.contains(societyName) return true;
 		 */
 		return false;
 	}
