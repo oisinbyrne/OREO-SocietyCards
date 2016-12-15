a46a1e1c73caeda4d6d68b712f41fab5a79303a3
Rucha <rucha.sohoni@ucdconnect.ie>
2016-12-15 17:58:24 +0000

    two methods for client class
    they have errors cos the server is not set up yet

--- Society Cards/src/basicServer/ClientClass.java
+++ Society Cards/src/basicServer/ClientClass.java
@@ -1,5 +1,38 @@
 package basicServer;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ClientClass {
 
+	public void sendStudentNumber(int studentNumber) {
+		//send student number to server
+	      try {
+	         Socket toServer = new Socket(my_serverHost, my_serverPort);
+	         PrintWriter out = new PrintWriter(toServer.getOutputStream(), true);
+	         // Write the message to the socket.
+	         out.println(studentNumber);
+	         out.close();
+	         toServer.close();
+	       } catch (SecurityException se) {
+	         se.printStackTrace();
+	      }
+	   }	
+	
+	
+	public void  getMemberships(){
+		//get the returned list of societies the student id is associated with.
+		ObjectInputStream ois = null;  
+        ois = new ObjectInputStream(toServer.getInputStream());
+        ArrayList<String> membershipList = new ArrayList<String>();
+		try {
+			membershipList = (ArrayList<String>)ois.readObject();
+			System.out.println("Memberships:\n " + membershipList);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
