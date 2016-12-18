45d3e5250808365d69c1d11d3636d246278b9356
Rucha <rucha.sohoni@ucdconnect.ie>
2016-12-18 13:58:57 +0000

    Removed unused stuff in ClientClass

--- Society Cards/src/basicServer/ClientClass.java
+++ Society Cards/src/basicServer/ClientClass.java
@@ -1,12 +1,9 @@
 package basicServer;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class ClientClass {
     private String name;
@@ -25,8 +22,6 @@ public class ClientClass {
             PrintWriter out = new PrintWriter(toServer.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(toServer.getInputStream()));
             // Write the message to the socket.
-            String message = command+"/"+arg;
-//            System.out.println("Sent message: " + message);
             out.println(command+"/"+arg);
             System.out.println(in.readLine());
             out.close();
